f8b0ee82bbd0b7ef8817a510e3285ede2ea72841
Guix <88253846+xGuix@users.noreply.github.com>
2022-04-28 18:08:12 +0200

    close branch mvc

--- TourGuide/src/test/java/tourGuide/TestRewardsService.java
+++ TourGuide/src/test/java/tourGuide/TestRewardsService.java
@@ -47,7 +47,7 @@ public class TestRewardsService {
 		assertTrue(rewardsService.isWithinAttractionProximity(attraction, attraction));
 	}
 	
-	// Needs fixed - can throw ConcurrentModificationException
+	//TODO: Needs fixed - can throw ConcurrentModificationException
 	@Test
 	public void nearAllAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
